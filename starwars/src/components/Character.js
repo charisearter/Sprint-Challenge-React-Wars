51f70f8589ba962305ed24c0dcb54ee3ebb5c3a2
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-05-15 11:23:39 -0500

    character setup but axios stopped working

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,23 @@
 // Write your Character component here
+import React from 'react';
+
+
+
+function Character (props) {
+    return (
+        
+        <div>
+            <img src={props.data.image} alt={props.data.name}/>
+            <h1>Name: {props.data.name}</h1>
+            <h2>Status: {props.data.status}</h2>
+            <p>Species: {props.data.species}</p>
+            <p>Type:{props.data.type}</p>
+            <p>Gender:{props.data.gender}</p>
+            <p>Has appeared in {props.data.episode.length()} episode(s)</p>
+
+        </div>
+    )
+}
+
+export default Character;
+
