9b7d65a27d7ea5df1490a57017338f1d0a69a11f
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-05-15 13:28:41 -0500

    still adjusting border styles

--- starwars/src/components/StyledDiv.js
+++ starwars/src/components/StyledDiv.js
@@ -5,9 +5,10 @@ const StyledDiv = styled.div `
     
     width: 40%;
     background-color: white;
+    border: 3px ridge black;
     border-radius: 25px;
     box-shadow: 5px 5px 5px #888;
-    padding: 3%;
+    padding: 2%;
     margin: 2% auto;
     text-align: left;
 `;
